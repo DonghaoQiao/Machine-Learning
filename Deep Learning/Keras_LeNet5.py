1b451f130540b4fbdd00ae71f25f8ac4ed104f5c
Donghao Qiao <qiaodonghao@gmail.com>
2019-10-11 10:02:51 -0400

    Update Keras_LeNet5.py

--- Deep Learning/Keras_LeNet5.py
+++ Deep Learning/Keras_LeNet5.py
@@ -4,19 +4,19 @@ class LeNet(models.Sequential):
     def __init__(self, input_shape, n_classes):
         super().__init__()
         # layer 1, conv layer
-        self.add(layers.Conv2D(filters=6, kernel_size=5, strides=1, activation='relu', input_shape=input_shape, padding='same'))
+        self.add(layers.Conv2D(filters=6, kernel_size=5, strides=1, activation='sigmoid', input_shape=input_shape, padding='same'))
         # layer 2, pooling layer
         self.add(layers.AveragePooling2D(pool_size=2, strides=1, padding='valid'))
         # layer 3, conv layer
-        self.add(layers.Conv2D(filters=16, kernel_size=5, strides=1, activation='relu', padding='valid'))
+        self.add(layers.Conv2D(filters=16, kernel_size=5, strides=1, activation='sigmoid', padding='valid'))
         # layer 4, pooling layer
         self.add(layers.AveragePooling2D(pool_size=2, strides=2, padding='valid'))
         # flatten the CNN output so that we can connect it with fully connected layers
         self.add(layers.Flatten())
         # layer 5, fully connected layer
-        self.add(layers.Dense(units=120, activation='relu'))
+        self.add(layers.Dense(units=120, activation='sigmoid'))
         # layer 6, fully connected layer
-        self.add(layers.Dense(units=84, activation='relu'))
+        self.add(layers.Dense(units=84, activation='sigmoid'))
         # layer 7, output layer
         self.add(layers.Dense(units=n_classes, activation='softmax'))
 
