95582d60afc7827bec372ec83116f9bd6886e0c4
Donghao Qiao <qiaodonghao@gmail.com>
2019-10-10 23:48:30 -0400

    Add files via upload

---
+++ Deep Learning/loadMNIST.py
@@ -0,0 +1,32 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from tensorflow import keras
+
+# load dataset
+(train_x_raw,train_y_raw),(test_x_raw,test_y_raw)=keras.datasets.mnist.load_data()
+def load_mnist():
+	# normalization
+	train_x = train_x_raw/255
+	test_x = test_x_raw/255
+	train_y = keras.utils.to_categorical(train_y_raw, 10)
+	test_y = keras.utils.to_categorical(test_y_raw, 10)
+	return(train_x,train_y,test_x,test_y)
+
+# plot an exampe
+def plot_mnist(i):
+	plt.imshow(train_x_raw[i], cmap='gray')
+	plt.title(train_y_raw[i])
+	plt.show()
+
+# plot misclassification
+def plot_miss(pred_y):
+    arr=[]
+    for i in range(test_y_raw.shape[0]):
+        if test_y_raw[i]!=pred_y[i]:
+            arr.append(i)
+    i=arr[np.random.randint(len(arr))]
+    plt.imshow(test_x_raw[i])
+    plt.title(('True: ', test_y_raw[i], 'Predict: ', pred_y[i]))
+    plt.show()
+
+    
