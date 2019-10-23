c773107885b50580fe244bd84208b7749883740f
Donghao Qiao <qiaodonghao@gmail.com>
2019-10-23 11:10:27 -0400

    Update Multi_Armed_Bandit.py

--- Reinforcement Learning/Multi_Armed_Bandit.py
+++ Reinforcement Learning/Multi_Armed_Bandit.py
@@ -1,35 +1,36 @@
 # Multi-Armed Badit with epsilon-greedy
-
 import numpy as np
 
-# Number of bandits
+# number of bandits
 k = 3
-# Our action values
+# action values
 Q = np.zeros(k)
 # This is to keep track of the number of times we take each action
 N = np.zeros(k)
-# Epsilon value for exploration
+# epsilon value for exploration
 eps = 0.1
-# Iterations
+# iterations
 iters=50000
-# True probability of winning for each bandit
+# true probability of winning for each bandit
 p_bandits = [0.45, 0.40, 0.80]
 def pull(a):
-    # Pull arm of bandit with index `i` and return 1 if win, else return 0.
+    # pull arm of bandit with index `i` and return 1 if win, else return 0.
     if np.random.rand() < p_bandits[a]:
         return 1
     else:
         return 0
 for i in range(iters):
     if np.random.rand() > eps:
-        # Take greedy action most of the time
+        # take greedy action most of the time
         a = np.argmax(Q)
     else:
-        # Take random action with probability eps
+        # take random action with probability eps
         a = np.random.randint(0, k)
-    # Collect reward
+    # collect reward
     reward = pull(a)
-    # Incremental average
+    # incremental average
     N[a] += 1
     Q[a] += 1/N[a] * (reward - Q[a])
+
+# Q will converge to the true probability
 print(Q)
