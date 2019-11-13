31ebc2f6829731bdc70f04603998a08cf1c77327
Donghao(Jeremy) Qiao <qiaodonghao@gmail.com>
2019-11-13 12:27:11 -0500

    Update K-Means.py

--- Machine Learning/Unsupervised Learning/K-Means.py
+++ Machine Learning/Unsupervised Learning/K-Means.py
@@ -1,95 +1,29 @@
-import random
-import math
+from sklearn import datasets
+from sklearn.cluster import KMeans
+from sklearn.cluster import DBSCAN
 
-#Euclidian Distance between two d-dimensional points
-def eucldist(p0,p1):
-    dist = 0.0
-    for i in range(0,len(p0)):
-        dist += (p0[i] - p1[i])**2
-    return math.sqrt(dist)
+# load data
+iris = datasets.load_iris()
+x = iris.data
+x = (x-x.min(axis=0))/(x.max(axis=0)-x.min(axis=0))
+y = iris.target
+fig = plt.figure(figsize=(15,5))
 
+# original distribution
+plt.subplot(131)
+plt.scatter([xi[2] for xi in x],[xi[3] for xi in x],c=y)
+plt.title('Origin')
 
-    
-#K-Means Algorithm
-def kmeans(k,datapoints):
+# K-means cluster
+plt.subplot(132)
+y_pred = KMeans(n_clusters=3).fit_predict(x)
+plt.scatter([xi[2] for xi in x],[xi[3] for xi in x],c=y_pred)
+plt.title('K-means')
 
-    # d - Dimensionality of Datapoints
-    d = len(datapoints[0]) 
-    
-    #Limit our iterations
-    Max_Iterations = 1000
-    i = 0
-    
-    cluster = [0] * len(datapoints)
-    prev_cluster = [-1] * len(datapoints)
-    
-    #Randomly Choose Centers for the Clusters
-    cluster_centers = []
-    for i in range(0,k):
-        new_cluster = []
-        #for i in range(0,d):
-        #    new_cluster += [random.randint(0,10)]
-        cluster_centers += [random.choice(datapoints)]
+# DBSCAN cluster
+plt.subplot(133)
+y_pred = DBSCAN(eps = 0.1, min_samples = 5).fit_predict(x)
+plt.scatter([xi[2] for xi in x],[xi[3] for xi in x],c=y_pred)
+plt.title('DBSCAN')
 
-        force_recalculation = False
-    
-    while (cluster != prev_cluster) or (i > Max_Iterations) or (force_recalculation) :
-        
-        prev_cluster = list(cluster)
-        force_recalculation = False
-        i += 1
-    
-        #Update Point's Cluster Alligiance
-        for p in range(0,len(datapoints)):
-            min_dist = float("inf")
-            
-            #Check min_distance against all centers
-            for c in range(0,len(cluster_centers)):
-                
-                dist = eucldist(datapoints[p],cluster_centers[c])
-                
-                if (dist < min_dist):
-                    min_dist = dist  
-                    cluster[p] = c   # Reassign Point to new Cluster
-        
-        
-        #Update Cluster's Position
-        for k in range(0,len(cluster_centers)):
-            new_center = [0] * d
-            members = 0
-            for p in range(0,len(datapoints)):
-                if (cluster[p] == k): #If this point belongs to the cluster
-                    for j in range(0,d):
-                        new_center[j] += datapoints[p][j]
-                    members += 1
-            
-            for j in range(0,d):
-                if members != 0:
-                    new_center[j] = new_center[j] / float(members) 
-                
-                #This means that our initial random assignment was poorly chosen
-                #Change it to a new datapoint to actually force k clusters
-                else: 
-                    new_center = random.choice(datapoints)
-                    force_recalculation = True
-                    print "Forced Recalculation..."
-                    
-            
-            cluster_centers[k] = new_center
-    
-        
-    print "======== Results ========"
-    print "Clusters", cluster_centers
-    print "Iterations",i
-    print "Assignments", cluster
-    
-    
-#TESTING THE PROGRAM#
-if __name__ == "__main__":
-    #2D - Datapoints List of n d-dimensional vectors. (For this example I already set up 2D Tuples)
-    #Feel free to change to whatever size tuples you want...
-    datapoints = [(3,2),(2,2),(1,2),(0,1),(1,0),(1,1),(5,6),(7,7),(9,10),(11,13),(12,12),(12,13),(13,13)]
-
-    k = 2 # K - Number of Clusters
-      
-    kmeans(k,datapoints) 
+plt.show()
