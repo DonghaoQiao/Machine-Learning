06fc0ee3f5d96d07e6942e074d00ac36d671aec1
Donghao(Jeremy) Qiao <qiaodonghao@gmail.com>
2019-11-17 15:32:10 -0500

    Update Gym_CartPole.py

--- Reinforcement Learning/Gym_CartPole.py
+++ Reinforcement Learning/Gym_CartPole.py
@@ -1,8 +1,13 @@
 import gym
 
 env = gym.make('CartPole-v0')
-print('observation size: ', env.observation_space) # observation size:  Box(4,)
-print('action size: ', env.action_space) # action size: Discrete(2)
+print('observation size: ', env.observation_space)
+#> observation size:  Box(4,)
+print(env.observation_space.high)
+print(env.observation_space.low)
+print('action size: ', env.action_space
+#> action size: Discrete(2)
+
 for _ in range(10):
     observation = env.reset()
     print(observation)
